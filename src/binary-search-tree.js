f68ea68a9d7a2ace421019ff16a6fd3fcc307146
Siarhei <siarhei.by@gmail.com>
2022-04-30 19:15:11 +0300

    feat: add some methods binary search tree

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,26 +1,57 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require('../extensions/list-tree.js');
 
 /**
 * Implement simple binary search tree according to task description
 * using Node from extensions
 */
 class BinarySearchTree {
+  constructor() {
+    this.rootNode = null;
+  }
 
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.rootNode;
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  add(data) {
+    this.rootNode = addInside(this.rootNode, data);
+
+    function addInside(node, value){
+      if(!node){
+        return new Node(value);
+      }
+      if(node.data === value){
+        return node;
+      }
+      if(value < node.data){
+        node.left = addInside(node.left, value);
+      }
+      if(value > node.data){
+        node.right = addInside(node.right, value);
+      }
+      return node;
+    }
   }
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  has(data) {
+    return searchInside(this.rootNode, data);
+
+    function searchInside(node, value){
+      if(!node){
+        return false;
+      }
+      if(node.data === value){
+        return true;
+      }
+      if(value < node.data){
+        return searchInside(node.left, value);
+      }
+      if(value > node.data){
+        return searchInside(node.right, value);
+      }
+    }
   }
 
   find(/* data */) {
@@ -34,13 +65,23 @@ class BinarySearchTree {
   }
 
   min() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if(!this.rootNode) return;
+
+    let minNode = this.rootNode;
+    while(minNode.left){
+      minNode = minNode.left;
+    }
+    return minNode.data;
   }
 
   max() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if(!this.rootNode) return;
+
+    let maxNode = this.rootNode;
+    while(maxNode.right){
+        maxNode = maxNode.right;
+    }
+    return maxNode.data;
   }
 }
 
